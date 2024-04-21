831e5240b22b33b34c1986283af578ce6f57165c
Marcilio Freitas <mvf1@discente.ifpe.edu.br>
2024-04-21 17:56:32 -0300

    Refatoração e correção de bugs

--- src/main/java/usuario/api/service/UsuarioService.java
+++ src/main/java/usuario/api/service/UsuarioService.java
@@ -9,23 +9,21 @@ import usuario.api.repository.UsuarioRepository;
 @Service
 public class UsuarioService {
 
+	@Autowired
     private UsuarioRepository repository;
 
-    @Autowired
+    
     public UsuarioService(UsuarioRepository usuarioRepository) {
         this.repository = usuarioRepository;
     }
 
     public Usuario cadastrarUsuario(Usuario usuario) {
-        // Aqui você pode adicionar lógica adicional antes de salvar o usuário,
-        // como criptografar a senha, validar os dados do usuário, etc.
+       
         return repository.save(usuario);
     }
 
     public Usuario autenticarUsuario(Usuario usuario) {
-        // Aqui você pode adicionar a lógica para autenticar o usuário.
-        // Isso pode envolver a busca do usuário no banco de dados pelo nome de usuário,
-        // a comparação da senha fornecida com a senha armazenada (depois de descriptografá-la, se necessário), etc.
+       
         Usuario usuarioEncontrado = repository.findByEmail(usuario.getEmail());
         if (usuarioEncontrado != null && usuarioEncontrado.getSenha().equals(usuario.getSenha())) {
             return usuarioEncontrado;
